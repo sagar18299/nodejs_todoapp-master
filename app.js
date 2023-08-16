7e7fa0c2beb5fd4224a67c8e0c4d1034db58dd30
sagar18299 <69413859+sagar18299@users.noreply.github.com>
2023-08-16 19:12:26 +0530

    header pass for cors

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(
     credentials: true,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Using routes
 app.use("/api/v1/users", userRouter);
