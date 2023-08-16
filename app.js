783ae132b49f0e9ca79606b9e043831bdec22efc
sagar18299 <69413859+sagar18299@users.noreply.github.com>
2023-08-16 19:24:09 +0530

    demo1

--- app.js
+++ app.js
@@ -15,18 +15,23 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "PUT", "POST", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "PUT", "POST", "DELETE"],
+//     credentials: true,
+//   })
+// );
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(cors({
+  origin: 'https://react-todo-app-master-p1ay-j8pj3wfxa-sagar18299.vercel.app',
+  credentials: true
+}));
+
 
 // Using routes
 app.use("/api/v1/users", userRouter);
